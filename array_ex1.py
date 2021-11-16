3047e46585191aadfa899d66a74114e3699b3c46
Pai Yang <zxcv255702001@gmail.com>
2021-11-16 20:44:01 +0800

    algorithm array ex1

--- array_ex1.py
+++ array_ex1.py
@@ -1,12 +1,12 @@
 from array import*
 
-x=array('i',[1,11,22,33,44,55])
+x=array('i',[1,11,22,33,44,55])  
 
 print("陣列內容如下:")
 for data in x:
     print(data)
 
-i=eval(input("請輸入欲刪除的索引:"))
+i=eval(input("請輸入欲刪除的索引:"))  
 
 if i<=5 and i>=0:
     x.pop(i)
